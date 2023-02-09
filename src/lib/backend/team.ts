4bfee9cc1b4b0c9b916e7f174fe7e266b324050a
resultfulness <alicja.rrosiak@gmail.com>
2023-02-09 13:51:07 +0100

    Implement initial /team

---
+++ src/lib/backend/team.ts
@@ -0,0 +1,160 @@
+export const management = [
+  {
+    imgSrc: "/img/team/2022_23/k_cetnar.jpg?size=400",
+    icon: "/img/icons/icons8-businesswoman-100.png",
+    title: "WICEPRZEWODNICZĄCA",
+    name: "Kalina Cetnar",
+    info: `Miłośniczka cięższych brzmień oraz naczelna basistka w Sekcji
+      Artystycznej. Uczennica czwartej klasy na profilu technik informatyk
+      dwujęzyczny - humanistka z serca, ścisłowiec z zawodu. Wyróżnia się
+      kreatywnością, stanowczością oraz otwartością na nowe doświadczenia i
+      pomysły 😉`,
+  },
+  {
+    imgSrc: "/img/team/2022_23/k_tyszkiewicz.jpg?size=400",
+    icon: "/img/icons/icons8-manager-100.png",
+    title: "PRZEWODNICZĄCY",
+    name: "Kornel Tyszkiewicz",
+    info: `Człowiek wielu talentów, filantrop, osoba nieszablonowa, wielowymiarowy,
+      nieosiągalny dla bytów tego świata, skromny ale zarazem lubiący pokazać
+      złoty perłowy pazur, kopalnia pomysłów, naszpikowany ekspresyjnym
+      wyrażaniem siebie twórca.`,
+  },
+  {
+    imgSrc: "/img/team/2022_23/p_stefanski.jpg?size=400",
+    icon: "/img/icons/icons8-businessman-100.png",
+    title: "WICEPRZEWODNICZĄCY",
+    name: "Piotr Stefański",
+    info: `Ambitny i odpowiedzialny człowiek, który jest ostoją spokoju samorządu.
+      Jeśli chciałbyś podyskutować na tematy mechatroniczne ale również mniej
+      spodziewane, jak żeglarstwo znajdziesz w Piotrku ciekawego rozmówcę.
+      Uwielbia sport, "Igrzyska śmierci", szachy, rocka z lat 80. i 90., nie
+      obce są mu zwierzęta i woda. Żegluje, pływa i strzela z łuku konno. Gdy
+      mówimy o jego artystycznym zacięciu, najważniejszym punktem jest gra na
+      gitarach: akustycznej i elektrycznej.`,
+  },
+  {
+    imgSrc: "/img/team/2022_23/n_simopulos.jpg?size=400",
+    icon: "/img/icons/icons8-chat-room-100.png",
+    title: "ZARZĄDCA POKOJU SU",
+    name: "Nikolaos Simopulos",
+    info: `Tegoroczny Zarządca Pokoju SU, mechatronik w klasie maturalnej. Osoba z
+      którą można porozmawiać na każdy możliwy temat, chętna do pomocy, o
+      sporym doświadczeniu w wielu dziedzinach. Artysta z krwi i kości,
+      miłośnik herbaty, wielki fan gotowania.`,
+  },
+  {
+    imgSrc: "/img/team/2019_20/img (3).jpg?size=400",
+    icon: "/img/icons/icons8-school-director-female-100.png",
+    title: "OPIEKUNKA SAMORZĄDU",
+    name: "Aneta Maciejczyk",
+    info: `Nauczycielka języka polskiego i wiedzy o kulturze z ponad 20-letnim
+      doświadczeniem. Praca z młodzieżą w szkole średniej, która od zawsze
+      była jej marzeniem, przynosi jej dużo satysfakcji i radości, które z
+      sukcesami przelewa na tych, których uczy. Jest otwarta na wyzwania i
+      czuje potrzebę zmieniania polskiej oświaty. Lubi eksperymentować z
+      edukacją i chętnie wspiera uczniów w ich nietuzinkowych pomysłach. Jest
+      pomysłodawczynią zastosowania metody Design Thinking na Wiśniowej.
+      Uważa, że najważniejszym elementem każdej pracy jest człowiek. Przykłada
+      dużą wagę do rozwijania wśród młodzieży umiejętności komunikacji,
+      kompetencji społecznych, empatii a także kształtowania relacji. Jest
+      jednym z tych nauczycieli, do których można przyjść z każdym problemem i
+      zawsze postara się znaleźć na ten problem rozwiązanie.`,
+  },
+  // {
+  //   imgSrc: "/img/team/2021_22/Wiktoria-Chudorlińska.jpg?size=400",
+  //   icon: "/img/icons/icons8-money-box-100.png",
+  //   title: "SKARBNIK SAMORZĄDU",
+  //   name: "Wiktoria Chudorlińska",
+  //   info: `Artyska, która nie boi się pracy z ludźmi. Otwarta na wyzwania i nowe, mimo że z pozoru bywa nieśmiała. Chętnie spędza
+  //     czas z ludźmi, chociaż lubi czasem pobyć sama ze sobą. Od pierwszych dni stawała się aktywnym członkiem samorządu
+  //     uczniowskiego. Pogodna, wyrozumiała i pracowita. Chce się rozwijać i zgłębiać tajemnice organizacji oraz zarządzania.
+  //     Jeśli masz ochotę na partyjkę lola czy apexa już wiesz do kogo się zgłosić.`
+  // }
+];
+
+export const chairpeople = [
+  {
+    imgSrc: "/img/team/2022_23/a_sliwczynski.jpg?size=400",
+    icon: "/img/icons/icons8-mind-map-100.png",
+    title: "SEKCJA ORGANIZACYJNA",
+    name: "Arkadiusz Śliwczyński",
+    info: `Ze względu na ułożenie planet oraz inne zdarzenia losowe opisu nie ma,
+      pojawi się niebawem...`,
+  },
+  {
+    imgSrc: "/img/team/2022_23/p_stefanski.jpg?size=400",
+    icon: "/img/icons/icons8-musical-notes-100.png",
+    title: "SEKCJA ARTYSTYCZNA",
+    name: "Piotr Stefański",
+    info: `Ambitny i odpowiedzialny człowiek, który jest ostoją spokoju samorządu.
+      Jeśli chciałbyś podyskutować na tematy mechatroniczne ale również mniej
+      spodziewane, jak żeglarstwo znajdziesz w Piotrku ciekawego rozmówcę.
+      Uwielbia sport, "Igrzyska śmierci", szachy, rocka z lat 80. i 90., nie
+      obce są mu zwierzęta i woda. Żegluje, pływa i strzela z łuku konno. Gdy
+      mówimy o jego artystycznym zacięciu, najważniejszym punktem jest gra na
+      gitarach: akustycznej i elektrycznej.`,
+  },
+  {
+    imgSrc: "/img/team/2022_23/w_nedew.jpg?size=400",
+    icon: "/img/icons/icons8-maintenance-100.png",
+    title: "SEKCJA TECHNICZNA",
+    name: "Wiktor Nedew",
+    info: `Z powołania programista, pasjonat technologii Open Source i Linuxa.
+      Żaden typ projektu nie jest mu straszny, chociaż preferuje C++ i
+      tworzenie Backendu aplikacji. Do każdego zadania podchodzi
+      odpowiedzialnie i profesjonalnie, nie lubi, gdy coś jest niedokończone.
+      W wolnym czasie fan gotowania oraz autor opowiadań. Miłośnik kryminału i
+      zawiłych historii z licznymi zwrotami akcji, czy to w grach, książkach,
+      czy w serialach lub filmach. Zawsze chętnie pomoże jak tylko będzie
+      mógł, wystarczy spytać.`,
+  },
+  {
+    imgSrc: "/img/team/2019_20/photo_unknown.jpg?size=400",
+    icon: "/img/icons/icons8-maintenance-100.png",
+    title: "SEKCJA TECHNICZNA",
+    name: "Michał Leśniowski",
+    info: `Ambitny, zabawny, lekko ekscentryczny i zawsze skory do pomocy w każdej
+      potrzebie. Oprócz webdevu interesuje się motoryzacją i rynkami
+      finansowymi. Ma otwarty umysł za sprawą którego, stara się poszerzać
+      swoje horyzonty rozmowami z różnymi ludźmi czy czytaniem książek o
+      samorozwoju. Skupia się na stawianiu sobie małych celów i metodycznym
+      wypełnianiu ich.`,
+  },
+  // {
+  //   imgSrc: "/img/team/2019_20/photo_8.jpg?size=400",
+  //   icon: "/img/icons/icons8-communication-100.png",
+  //   title: "SEKCJA DEBATANCKA",
+  //   name: "Bartłomiej Biadoń",
+  //   info: `Jest jedną z tych osób, które zostały stworzone na swoje stanowisko. Kocha debaty i dyskusje. Uważa,
+  //     że najwyższą formą posiadania własnego zdania, jest umiejętność rozmawiania o nim i bronienia go. Na
+  //     problemy patrzy na wielu płaszczyznach i w każdej jest w stanie znaleźć mnóstwo wad i zalet
+  //     konkretnego rozwiązania. Interesuje się relacjami międzyludzkimi, psychologią, ekonomią i polityką.`,
+  // },
+  {
+    imgSrc: "/img/team/2022_23/m_lukasiak.jpg?size=400",
+    icon: "/img/icons/icons8-job-seeker-100.png",
+    title: "SEKCJA PRAWNA",
+    name: "Michał Łukasiak",
+    info: `Pasjonat ekonomii, przedsiębiorczości i związanego z nimi prawa.
+      Innowator, który chętnie zaangażuje się w Twój projekt i pomoże w
+      uzyskaniu niezbędnych środków i partnerów, w czym ma już spore
+      doświadczenie. Poza szkołą Wiceprzewodniczący oraz p.o. Sekretarza
+      Młodzieżowej Rady Dzielnicy Mokotów, a także członek Komisji Rewizyjnej
+      Młodzieżowej Rady m.st. Warszawy. Prywatnie lubi dobrze zjeść i
+      żeglować, a w swoje pasje (obie) lubi wciągać znajomych!`,
+  },
+  {
+    imgSrc: "/img/team/2022_23/w_perzanowska.jpg?size=400",
+    icon: "/img/icons/icons8-social-network-100.png",
+    title: "SEKCJA MEDIALNA",
+    name: "Wiktoria Perzanowska",
+    info: `Ekstrawertyk z dużym poczuciem humoru i dystansu do siebie. Ambitna
+      optymistka, dla której nie ma problemu, którego nie dałoby się w jakiś
+      sposób rozwiązać. Jeśli potrzebujesz porady w jakiejkolwiek dziedzinie,
+      zawsze służy pomocą. Człowiek, z którym możesz dyskutować na każdy
+      możliwy temat. Ma głowę pełną pomysłów, które stara się zrealizować w
+      jak najlepszym wydaniu. Prywatnie fanka gotowania, miłośniczka książek
+      kryminalnych, filmów akcji i hard rocka.`,
+  },
+];
---
+++ src/routes/team/HumanCard.svelte
@@ -0,0 +1,208 @@
+<script lang="ts">
+  export let person: {
+    imgSrc: string;
+    icon: string;
+    title: string;
+    name: string;
+    info: string;
+  };
+
+  const { title, name, info, imgSrc, icon } = person;
+
+  let isOpened = false;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  class="human-card"
+  class:open={isOpened}
+  class:disp={isOpened}
+  on:click={() => (isOpened = isOpened === false ? true : false)}
+  on:mouseleave={() => (isOpened = false)}
+>
+  <img src={imgSrc} alt={name} class="personimg" />
+  <div class="info">
+    <img src={icon} alt="" />
+    <div class="box">
+      <p class="title">{title}</p>
+      <p class="name">{name}</p>
+    </div>
+  </div>
+  <div class="moreinfo">
+    {info}
+  </div>
+</div>
+
+<style lang="scss">
+  .human-card {
+    width: 350px;
+    height: 400px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    margin: 20px;
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: 300px 100px;
+    grid-column-gap: 0;
+    grid-row-gap: 0;
+    transform-style: preserve-3d;
+    border-radius: 5px;
+    cursor: pointer;
+
+    &.open:not(.hover) {
+      .moreinfo {
+        opacity: 1;
+        background-color: white;
+        margin-top: 19px;
+        transform: translate3d(0, -6px, 30px);
+      }
+
+      .info {
+        border-radius: 0;
+        margin: 11px 11px 7px 11px !important;
+        padding-bottom: 4px;
+        transform: translate3d(0, -6px, 30px);
+
+        @supports (-ms-ime-align: auto) {
+          margin: 11px 11px 11px 11px !important;
+          padding-bottom: 0;
+          transform: translate3d(0, 0, 30px);
+        }
+      }
+    }
+
+    &.open.hover {
+      .moreinfo {
+        opacity: 1;
+        background-color: white;
+        margin: 10px 8px -15px 8px;
+        transform: translateZ(30px);
+      }
+    }
+
+    &.open {
+      transform: scale(1.1) !important;
+    }
+
+    .moreinfo {
+      grid-area: 1 / 1 / 2 / 2;
+      z-index: 3;
+      opacity: 0;
+      margin: 312px 11px -12px 11px;
+      padding: 15px;
+      transition: all 0.2s ease-in-out;
+      overflow-y: scroll;
+      -webkit-overflow-scrolling: touch;
+
+      @supports (-ms-ime-align: auto) {
+        margin: 319px 11px -19px 11px;
+      }
+    }
+
+    .info {
+      margin: 15px;
+      grid-area: 2 / 1 / 3 / 2;
+      z-index: 2;
+      background-color: white;
+      border-radius: 5px;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+
+      transition: all 0.2s ease-in-out;
+
+      display: grid;
+      grid-template-columns: 70px 1fr;
+      grid-template-rows: auto;
+      grid-column-gap: 0;
+      grid-row-gap: 0;
+
+      overflow: hidden;
+
+      img {
+        grid-area: 1 / 1 / 2 / 2;
+        width: calc(100% - 30px);
+        height: calc(100% - 30px);
+        padding: 15px;
+        object-position: center;
+        object-fit: contain;
+        filter: drop-shadow(0 0 10px rgba(0, 0, 0, 0.15));
+        pointer-events: none;
+        overflow: hidden;
+      }
+
+      div.box {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        justify-content: center;
+        align-content: flex-start;
+        align-items: flex-start;
+        // fixes a weird bug when sometimes the height doesn't automatically set
+        // to 70px and stays 100px until hover event happens
+        height: 70px;
+        padding-left: 10px;
+      }
+
+      p {
+        &.title {
+          font-family: "Raleway", sans-serif;
+          margin: 0;
+          font-size: 13px;
+          font-weight: 600;
+
+          color: rgba(0, 0, 0, 0.7);
+
+          @media (max-width: 425px) {
+            font-size: 10px;
+          }
+        }
+
+        &.name {
+          margin: 0;
+          font-size: 20px;
+          font-weight: 600;
+
+          @media (max-width: 425px) {
+            font-size: 18px;
+          }
+        }
+      }
+    }
+
+    img.personimg {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+      object-position: center;
+      grid-area: 1 / 1 / 3 / 2;
+      z-index: 1;
+      pointer-events: none;
+    }
+
+    &.hover {
+      .info {
+        border-radius: 0;
+        margin: 8px;
+        transform: translate3d(0, 0, 30px);
+      }
+
+      transform: scale(1.08) !important;
+    }
+  }
+
+  @media (hover: hover) {
+    .human-card:hover {
+      .info {
+        border-radius: 0;
+        margin: 11px;
+        transform: translate3d(0, -6px, 30px);
+
+        @supports (-ms-ime-align: auto) {
+          transform: translate3d(0, 0, 30px);
+        }
+      }
+
+      @supports (-ms-ime-align: auto) {
+        transform: scale(1.05) !important;
+      }
+    }
+  }
+</style>
